d586dc536b8f620f345e9a16f517ce0bdcdd9f4b
ktlh <laciedreamer@gmail.com>
2020-06-24 07:56:49 +0300

    perf

--- init.sql
+++ init.sql
@@ -1,5 +1,15 @@
 CREATE EXTENSION IF NOT EXISTS citext;
 
+ALTER SYSTEM SET max_connections = '200';
+ALTER SYSTEM SET shared_buffers = '512B';
+ALTER SYSTEM SET effective_cache_size = '768MB';
+ALTER SYSTEM SET checkpoint_completion_target = '0.9';
+ALTER SYSTEM SET random_page_cost = '1.1';
+ALTER SYSTEM SET effective_io_concurrency = '200';
+ALTER SYSTEM SET wal_buffers = '6912kB';
+ALTER SYSTEM SET default_statistics_target = '100';
+ALTER SYSTEM SET seq_page_cost = '0.1';
+ALTER SYSTEM SET random_page_cost = '0.1';
 
 /*CREATE DATABASE forum
     WITH
